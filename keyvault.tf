7529df1d6e7dd97c33cde3d412be62dc0c4a269b
Arnaud Lheureux <arnaudlh@hotmail.com>
2019-12-17 14:02:57 +0800

    Removing consecutive hyphens if no postfix

--- keyvault.tf
+++ keyvault.tf
@@ -4,7 +4,7 @@ locals {
     kv = {
         passthrough = (var.type == "kv" && var.convention == "passthrough") ? substr(local.filtered_extend.alphanumh_startletter, 0, local.max) : null
         cafclassic  = (var.type == "kv" && var.convention == "cafclassic") ? substr("${lookup(var.caf_prefix, var.type)}${local.filtered.alphanumh}-${local.filteredpostfix.alphanumh}", 0, local.max) : null
-        cafrandom   = (var.type == "kv" && var.convention == "cafrandom") ? substr("${lookup(var.caf_prefix, var.type)}${local.filtered.alphanumh}-${local.filteredpostfix.alphanumh}-${local.fullyrandom_startletter}", 0, local.max) : null 
+        cafrandom   = (var.type == "kv" && var.convention == "cafrandom") ? substr("${lookup(var.caf_prefix, var.type)}${local.filtered.alphanumh}-${local.filteredpostfix.alphanumh}${local.fullyrandom_startletter}", 0, local.max) : null 
         random      = (var.type == "kv" && var.convention == "random") ? substr(local.fullyrandom_startletter, 0, local.max) : null
     }
  }
