5e9303201dda95335d67ef14e5425d449ec71b28
Arnaud Lheureux <arnaudlh@hotmail.com>
2020-01-02 19:24:17 +0800

    evh fix

--- eventhub.tf
+++ eventhub.tf
@@ -5,7 +5,7 @@ locals {
       passthrough = (var.type == "evh" && var.convention == "passthrough") ? substr(local.filtered.alphanumhu, 0, local.max) : null
       cafclassic  = (var.type == "evh" && var.convention == "cafclassic") ? substr("${lookup(var.caf_prefix, var.type)}${local.filtered.alphanum}${local.filteredpostfix.alphanum}", 0, local.max) : null
       cafrandom   = (var.type == "evh" && var.convention == "cafrandom") ? substr("${lookup(var.caf_prefix, var.type)}${local.filtered.alphanum}${local.filteredpostfix.alphanum}${local.fullyrandom}", 0, local.max) : null 
-      random      = (var.type == "evh" && var.convention == "random") ? substr(local.fullyrandom, 0, local.max) : null
+      random      = (var.type == "evh" && var.convention == "random") ? substr(local.fullyrandom_startletter, 0, local.max) : null
     }
  }
 output "evh" {
